6880b21184519dda1a51bc621de20e9621eccdb1
Arpit Pandey <arpitpandey1181@gmail.com>
2024-09-01 02:53:33 +0530

    add quarySelector code

--- JS/dom/byTagName.js
+++ JS/dom/byTagName.js
@@ -1,12 +1,13 @@
-let val = document.getElementsByTagName("h1")[0];
+let val = document.getElementsByTagName('h1');
 console.log(val);
 
 function myfun (){
     let num = document.getElementsByTagName("p");
-    setTimeout(() =>{
-        for(let i = 0; i < num.length; i++){
+    for(let i = 0; i < num.length; i++){
+        setTimeout(() =>{
         num[i].style.backgroundColor = "green";
         num[i].style.color = "white";
-        }
-    }, 2000);
+        }, 2000);
+    }
+    
 }
---
+++ JS/dom/querySelector.js
@@ -0,0 +1,17 @@
+let x = document.querySelector("h1");
+console.log(x);
+
+let y = document.querySelector(".class_1");
+console.log(y);
+
+let z = document.querySelector("#div_id");
+console.log(z);
+
+let a = document.querySelectorAll("p")[0];
+console.log(a);
+
+let b = document.querySelector(".class_h4");
+console.log(b);
+
+let c = document.querySelector("#div_id");
+console.log(c);
