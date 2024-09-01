81b870a9074040d37a81c077e26512124ea691da
Arpit Pandey <arpitpandey1181@gmail.com>
2024-09-01 13:42:20 +0530

    dom proparty

---
+++ JS/dom/domProparty.js
@@ -0,0 +1,17 @@
+let a = document.querySelector("#div_id");
+console.log(a.tagName);
+
+let b = document.querySelector(".class_1").tagName;
+console.log(b);
+
+let c = document.querySelector("#div_id") ;
+console.log(c.innerHTML);
+
+let d = document.querySelector("h1").innerHTML = "i love js" ;
+console.log(d);
+
+let e = document.querySelector("#divid").innerText ;
+console.log(e);
+
+let f = document.querySelector(".class_h5").textContent ;
+console.log(f);
