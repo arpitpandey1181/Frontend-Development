a76517298f193e24586e0ecd5f135660b81e9441
Arpit Pandey <arpitpandey1181@gmail.com>
2024-09-02 13:48:22 +0530

    add attributes code

---
+++ JS/dom/attributes.js
@@ -0,0 +1,10 @@
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+
+let b = document.querySelector("p");
+let a = b.setAttribute("class","myClass");
+console.log(a);
