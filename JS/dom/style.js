3115ee36c82edb04cf6eaba2fa830039dc5857b4
Arpit Pandey <arpitpandey1181@gmail.com>
2024-09-02 23:56:11 +0530

    style with js

---
+++ JS/dom/style.js
@@ -0,0 +1,10 @@
+let h = document.querySelector("h1");
+h.style.backgroundColor = "pink";
+h.style.border = "solid";
+h.style.borderBlockColor = "black";
+h.style.textAlign = "center"
+
+let p = document.querySelector("p");
+p.style.color = "blue";
+p.style.textAlign = "center";
+p.style.fontWeight = " bold";
