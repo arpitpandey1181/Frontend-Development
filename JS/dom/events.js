72e46a7e242e3f7cdd38354db5351cd74b6d2652
Arpit Pandey <arpitpandey1181@gmail.com>
2024-09-06 20:52:46 +0530

    event codes

--- JS/dom/events.js
+++ JS/dom/events.js
@@ -7,7 +7,42 @@ btn1.onclick = () =>{
     console.log(a);
 }
 
+btn1.onclick = () =>{
+    console.log("hello js 2 :-");
+}
+
 let div = document.querySelector("div");
 div.onmouseover = () =>{
     console.log("you are inside div...!");
+}
+
+//Event Object :-
+
+let btn2 = document.querySelector("#btn_2");
+
+btn2.onclick = (e) =>{
+    console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+    console.log(e.clientX, e.clientY); 
+
+    //Event Listener :-
+
+    let btn3 = document.querySelector("#btn_3");
+
+    btn3.addEventListener("click", ()=>{
+        console.log("button 3 was clicked ...");
+    });
+    btn3.addEventListener("click", ()=>{
+        console.log("button 3 was clicked again ...");
+    });
+    const remove = ()=>{
+        console.log("button 3 was clicked ...2");
+    };
+    // btn3.addEventListener("click", (e)=>{
+    //     console.log(e);
+    //     console.log(e.type);
+    // });
+
+    btn3.removeEventListener("click", remove);
 }
