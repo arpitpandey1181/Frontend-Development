6eb85cf2ac5ea0a33e88e88c455966673a941306
Arpit Pandey <arpitpandey1181@gmail.com>
2024-09-13 17:09:59 +0530

    new change

--- JS/oops/object.js
+++ JS/oops/object.js
@@ -1,7 +1,23 @@
 //OBJECT LITEREL :-
 
 let person = {
-    first_Name : 'Arpit',
-    last_Name : 'Pandey'
+    first_Name: 'Arpit',
+    last_Name: 'Pandey',
+
+    // method
+    getfunction: function() {
+        return `The name of the person is ${person.first_Name} ${this.last_Name}`;
+    },
+
+    // OBJECT WITHIN OBJECT
+    phone_number : {
+        mobile: '12345',
+        landline: '07662'
+    }
 }
+
+console.log(person);
 console.log(person.first_Name);
+console.log(person.phone_number.landline);
+console.log(person.getfunction()); 
+
